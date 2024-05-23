54523505d9a4fb236b35c84f4c4130f098def5f8
Danila Shuvaev <dshuvaev@art3d.ru>
2024-05-23 18:09:57 +0300

    f5

--- components/Authorisation.vue
+++ components/Authorisation.vue
@@ -1,39 +1,61 @@
 <template>
     <div class="auth">
         <h1>Авторизация</h1>
-        <form>
-            <input type="text" method="post" name="login" placeholder="Логин" />
-            <input type="text" method="post" name="password" placeholder="Пароль" />
+        <form ref="authForm" @submit.prevent="validateForm" autocomplete="off">
+            <input type="text" name="login" placeholder="Логин" />
+            <input type="password" name="password" placeholder="Пароль" />
+            <button>
+                <div>Войти</div>
+            </button>
         </form>
     </div>
 </template>
 
 <script>
 
+export default {
+    methods: {
+        async validateForm() {
+            await fetch('/api/auth', {
+                method: 'POST',
+                body: new FormData(this.$refs.authForm),
+            })
+                .then(resp => resp.json())
+                .then(data => {
+                    console.log(data);
+                });
+        },
+    },
+};
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .auth {
     display: flex;
     flex-direction: column;
     justify-content: center;
     margin-top: 2.5rem;
     gap: 1.5rem;
-}
-h1 {
-    text-align: center;
-    font-family: "Rubik";
-    font-size: 4rem;
-}
-form {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    & input {
-        width: 30rem;
-        height: 3rem;
-        padding: 0.5rem;
-        border-radius: 0.5rem;
+        
+    h1 {
+        text-align: center;
+        font-family: "Rubik";
+        font-size: 4rem;
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        & input {
+            width: 30rem;
+            height: 3rem;
+            padding: 0.5rem;
+            border-radius: 0.5rem;
+        }
+        & button div {
+            padding: 0.5rem;
+            border-radius: 0.5rem;
+        }
     }
 }
 </style>
--- server/api/auth/index.post.ts
+++ server/api/auth/index.post.ts
@@ -1,8 +1,25 @@
+interface form {
+    'login': string,
+    'password': string
+}
+
 export default defineEventHandler(async event => {
-    const body = await readBody(event);
-    if (body) {
-        return (body)
-    } else {
-        return 'Empty request'
+    const body = await readMultipartFormData(event) ?? [];
+    console.log(body);
+
+    const formData: form = body.reduce((acc, cur) => {
+        acc[cur['name']] = cur.data.toString();
+        return acc
+    }, {})
+
+    if (formData?.login && formData?.password) {
+        return {
+            ...formData,
+            status: 1,
+        };
     }
+
+    return {
+        status: 0,
+    };
 })
